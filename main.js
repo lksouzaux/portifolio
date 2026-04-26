0effefc88eed038c051e0b375eddd1f186b69c78
Lucas Souza <lksouza.ux@gmail.com>
2026-04-26 19:12:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const typed = new Typed(".text", {
+    strings: ["Dev FrontEnd", "Web Designer", "UI/UX Designer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
